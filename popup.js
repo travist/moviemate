cf7789b27c6085ba9b96b6cf690c36f8a7ae6fa4
Travis Tidwell <travist349@gmail.com>
2013-03-14 10:14:01 -0500

    Cleaning up the popup.js

--- popup.js
+++ popup.js
@@ -3,37 +3,24 @@ $(function() {
   // Say we are loading.
   $('body').addClass('ui-loading');
 
+  var list = $('#home ul'),
+      item = null,
+      a = null,
+      title = null,
+      desc = null,
+      image = null;
+
   // Get the playlist.
-  $.get('https://gdata.youtube.com/feeds/api/users/trailers/playlists', {
-    'v': 2,
-    'alt': 'json'
-  }, function(playlist) {
+  $.get('https://gdata.youtube.com/feeds/api/users/trailers/playlists?v=2&alt=json', function(playlist) {
 
-    // Remove the loading cursor.
-    $('body').removeClass('ui-loading');
-
-    // Get the list.
-    var list = $('#home ul'),
-        item = null,
-        a = null,
-        entry = null,
-        image = null,
-        title = null,
-        desc = null;
-
-    // Iterate through our playlist.
-    for (var i in playlist.feed.entry) {
-
-      // Get the entry from the playlist.
-      entry = playlist.feed.entry[i];
+    // Iterate over each entry.
+    $.each(playlist.feed.entry, function(index, entry) {
 
       // Create the list item.
       item = $(document.createElement('li'));
 
       // Set the link item, and bind to the click event.
-      a = $(document.createElement('a')).attr({
-        'href': '#'
-      }).click((function(url) {
+      a = $(document.createElement('a')).click((function(url) {
         return function(event) {
           event.preventDefault();
           chrome.tabs.create({'url': url});
@@ -41,26 +28,24 @@ $(function() {
       })(entry.link[1].href));
 
       // Get the title.
-      title = $(document.createElement('h3')).attr({
-        'class': 'ui-li-heading'
-      }).text(entry.title.$t);
+      title = $(document.createElement('h3')).text(entry.title.$t);
 
       // Get the description.
-      desc = $(document.createElement('p')).attr({
-        'class': 'ui-li-desc'
-      }).text(entry.summary.$t);
+      desc = $(document.createElement('p')).text(entry.summary.$t);
 
       // Get the image.
       image = $(document.createElement('img')).attr({
-        'src': entry.media$group.media$thumbnail[0].url,
-        'class': 'ui-li-thumb ui-corner-all'
+        'src': entry.media$group.media$thumbnail[0].url
       });
 
       // Add the item to the list.
       list.append(item.append(a.append(image).append(title).append(desc)));
-    }
+    });
 
-    // Create the listview
+    // Refresh the listview.
     list.listview('refresh');
+
+    // Remove the loading cursor.
+    $('body').removeClass('ui-loading');
   });
 });
